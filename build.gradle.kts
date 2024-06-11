98d44857c89c4a0651395c319b77cfc0a077a928
fugu <ffqwerty3@gmail.com>
2024-06-12 00:43:27 +0300

    add first handler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
+    application
+}
+
+application {
+    mainClass.set("org.example.hexlet.HelloWorld")
 }
 
 group = "org.example"
@@ -10,10 +18,21 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
