3a315627f750dc6270ac91fc73c6a85504cb47ce
fugu <ffqwerty3@gmail.com>
2024-06-13 22:53:16 +0300

    add path param handler

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -13,6 +13,12 @@ public class HelloWorld {
             context.result("Hello, " + name + "!");
         });
 
+        app.get("/users/{id}/post/{postId}", context -> {
+            var userId = context.pathParam("id");
+            var postId = context.pathParam("postId");
+            context.result(String.format("First param: %s\nSecond param: %s", userId, postId));
+        });
+
         app.start(7070);
     }
 }
