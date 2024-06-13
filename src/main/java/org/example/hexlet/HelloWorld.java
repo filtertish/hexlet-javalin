e748066719bae4111f1f5f14664fba32d0ac4b3f
fugu <ffqwerty3@gmail.com>
2024-06-13 20:28:21 +0300

    add query param handler

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -6,7 +6,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(javalinConfig -> javalinConfig.bundledPlugins.enableDevLogging());
 
-        app.get("/", context -> context.result("Hello World!"));
+        app.get("/", context -> context.result("Main page!"));
+
+        app.get("/hello", context -> {
+            var name = context.queryParamAsClass("name", String.class).getOrDefault("World");
+            context.result("Hello, " + name + "!");
+        });
+
         app.start(7070);
     }
 }
